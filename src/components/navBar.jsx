cc37605af972c6b0b0574200506293ae87c7894d
Michael Aldrich Serrano <mykeserrano@Michaels-iMac.local>
2019-11-14 21:04:15 -0500

    adding logout

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,45 +1,68 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
   return (
-    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-      <Link className='navbar-brand' to='#'>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="#">
         Video City
       </Link>
       <button
-        className='navbar-toggler'
-        type='button'
-        data-toggle='collapse'
-        data-target='#navbarNav'
-        aria-controls='navbarNav'
-        aria-expanded='false'
-        aria-label='Toggle navigation'
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
       >
-        <span className='navbar-toggler-icon'></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div className='collapse navbar-collapse' id='navbarNav'>
-        <ul className='navbar-nav'>
-          <li className='nav-item active'>
-            <NavLink className='nav-link' to='/movies'>
-              Movies <span className='sr-only'>(current)</span>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <NavLink className="nav-link" to="/movies">
+              Movies <span className="sr-only">(current)</span>
             </NavLink>
           </li>
-          <li className='nav-item'>
-            <NavLink className='nav-link' to='/customers'>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/customers">
               Customers
             </NavLink>
           </li>
-          <li className='nav-item'>
-            <NavLink className='nav-link' to='/rentals'>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/rentals">
               Rentals
             </NavLink>
           </li>
-          <li className='nav-item'>
-            <NavLink className='nav-link' to='/login'>
-              Login
-            </NavLink>
-          </li>
+          {!user && (
+            <React.Fragment>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/register">
+                  Register
+                </NavLink>
+              </li>
+            </React.Fragment>
+          )}
+          {user && (
+            <React.Fragment>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/profile">
+                  {user.name}
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/logout">
+                  Logout
+                </NavLink>
+              </li>
+            </React.Fragment>
+          )}
         </ul>
       </div>
     </nav>
--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -24,7 +24,17 @@ class RegisterForm extends Form {
   };
 
   doSubmit = async () => {
-    await userService.register(this.state.data);
+    try {
+      const response = await userService.register(this.state.data);
+      localStorage.setItem("token", response.headers["x-auth-token"]);
+      window.location = "/";
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
