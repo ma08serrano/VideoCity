89bc7b2ae0767e897278c23625ce7875b431ffdb
Michael Aldrich Serrano <mykeserrano@Michaels-iMac.local>
2019-11-22 13:14:08 -0500

    redesigned login page

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,55 +3,57 @@ import { Link, NavLink } from "react-router-dom";
 
 const NavBar = ({ user }) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="#">
-        Video City
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="nav navbar-nav">
-          {user && (
-            <React.Fragment>
-              <li className="nav-item active">
-                <NavLink className="nav-link" to="/movies">
-                  Movies <span className="sr-only">(current)</span>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/customers">
-                  Customers
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/rentals">
-                  Rentals
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/profile">
-                  {user.name}
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/logout">
-                  Logout
-                </NavLink>
-              </li>
-            </React.Fragment>
-          )}
-        </ul>
-      </div>
-    </nav>
+    <div className="mt-5">
+      {user && (
+        <React.Fragment>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="#">
+              Video City
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="nav navbar-nav">
+                <li className="nav-item active">
+                  <NavLink className="nav-link" to="/movies">
+                    Movies <span className="sr-only">(current)</span>
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/customers">
+                    Customers
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/rentals">
+                    Rentals
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/profile">
+                    {user.name}
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/logout">
+                    Logout
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </React.Fragment>
+      )}
+    </div>
   );
 };
 
